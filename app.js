d9db9c907c19c097c29e2c45d5d0fcd7cee55b99
Nataliia Gospodarikova <gospodarikovan@yandex.ru>
2023-06-12 00:43:53 +0300

    Исправление кода ошибки обработки неправильного пути

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use('/cards', cardRouter);
 app.use('/users', userRouter);
 app.use('/*', (req, res) => {
-  res.status(400).send({ message: 'Страница не найдена' });
+  res.status(404).send({ message: 'Страница не найдена' });
 });
 
 app.listen(PORT);
