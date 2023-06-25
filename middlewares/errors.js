55fa55b326f971d5de1404c1c46df29296719ccf
Nataliia Gospodarikova <gospodarikovan@yandex.ru>
2023-06-26 00:21:25 +0300

    14 спринт / корректировка синтаксиса

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,4 +1,8 @@
 module.exports = ((err, req, res, next) => {
-  res.status(err.statusCode ? err.statusCode : 500).send({ message: err.message });
+  if (err.message === 'Validation failed') {
+    res.status(err.statusCode ? err.statusCode : 400).send({ message: err.message });
+  } else {
+    res.status(err.statusCode ? err.statusCode : 500).send({ message: err.message });
+  }
   next();
 });
