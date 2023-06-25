56c16e9c63b527fdb81de7175198a95a62a99915
Nataliia Gospodarikova <gospodarikovan@yandex.ru>
2023-06-25 23:42:39 +0300

    14 спринт

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const {celebrate, Joi} = require("celebrate");
+const { celebrate, Joi } = require('celebrate');
 
 const cardRouter = require('./routes/cards');
 const userRouter = require('./routes/users');
 const authMiddleware = require('./middlewares/auth');
 const errorsMiddleware = require('./middlewares/errors');
 const userController = require('./controllers/users');
-const NotFoundError = require("./errors/not-found-err");
+const NotFoundError = require('./errors/not-found-err');
 
 const regex = /^(https?:\/\/)?[^\s]*/;
 
@@ -22,15 +22,17 @@ app.use(cookieParser());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.post('/signin',
+app.post(
+  '/signin',
   celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email().min(3),
       password: Joi.string().required().min(3),
     }),
   }),
-  userController.login);
-app.post('/signup',celebrate({
+  userController.login,
+);
+app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email().min(3),
     password: Joi.string().required().min(3),
@@ -45,7 +47,7 @@ app.use(authMiddleware.auth);
 app.use('/cards', cardRouter);
 app.use('/users', userRouter);
 
-app.use('/*', (req, res) => {
+app.use('/*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
 
